9a34395eb37175fa102beece47a9b7d8e4c0a017
Ben Wynn <bwynn@glowie.com>
2025-11-12 13:46:26 -0500

    When I stopped compiling with ASO_IMDCT, it started defining fastsdct which threw a warning because half the time y[18] is being handed in as y+1 and if you try and reference the the 18th element of that, you will have walked off the end of the array.  it never does that, so i could have suppressed the error, but instead chose to alter the function signature

--- libmad/layer3.c
+++ libmad/layer3.c
@@ -1576,8 +1576,9 @@ void III_aliasreduce(mad_fixed_t xr[576], int lines)
 void III_imdct_l(mad_fixed_t const [18], mad_fixed_t [36], unsigned int);
 # else
 #  if 1
+/* the argument y will be handed a mad_fixed_t[18] or mad_fixed_t[18]+1; so we can't assume we can write to the last elemenet */
 static
-void fastsdct(mad_fixed_t const x[9], mad_fixed_t y[18])
+void fastsdct(mad_fixed_t const x[9], mad_fixed_t y[17])
 {
   mad_fixed_t a0,  a1,  a2,  a3,  a4,  a5,  a6,  a7,  a8,  a9,  a10, a11, a12;
   mad_fixed_t a13, a14, a15, a16, a17, a18, a19, a20, a21, a22, a23, a24, a25;
