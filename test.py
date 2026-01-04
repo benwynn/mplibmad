2b67befb8f37b2f32ea38723588caf30fa001d0a
Ben Wynn <bwynn@glowie.com>
2026-01-04 13:03:55 -0500

    pulling some tests out into functions so i don't have to run them every time as i'm working...

--- test.py
+++ test.py
@@ -4,20 +4,22 @@ print("start:")
 
 mplibmad.hello("abc")
 
-print("testing with integer instead of string:")
-try:
-  mplibmad.hello(42)
-except TypeError:
-  print(f"got expected TypeError exception, working as designed")
-except Exception as e:
-  print(f"got unexpected exception: {type(e).__name__}: {e.value}")
+def testTypeError():
+    print("testing with integer instead of string:")
+    try:
+      mplibmad.hello(42)
+    except TypeError:
+      print(f"got expected TypeError exception, working as designed")
+    except Exception as e:
+      print(f"got unexpected exception: {type(e).__name__}: {e.value}")
 
 def my_callback(tmp):
     print("in my_callback")
     print(f"tmp was: {tmp}")
     return 123
 
-mplibmad.set_cb(my_callback)
-mplibmad.call_cb(42)
+def testCallback():
+    mplibmad.set_cb(my_callback)
+    mplibmad.call_cb(42)
 
 print("Done.")
