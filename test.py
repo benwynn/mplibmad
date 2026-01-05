4da6bb7306621768c62bfe103c71122670a3181d
Ben Wynn <bwynn@glowie.com>
2026-01-04 23:57:04 -0500

    improve test rigging

--- test.py
+++ test.py
@@ -1,25 +1,74 @@
-import mplibmad
+import mplibmad # type: ignore
+import time
 
-print("start:")
+class EnterExitLog():
+    def __init__(self, name):
+        self.name = name
 
-mplibmad.hello("abc")
+    def __enter__(self):
+        #print(f"Entering {self.name}")
+        self.init_time = time.time()
+        return self
 
-def testTypeError():
-    print("testing with integer instead of string:")
+    def set_result(self, result):
+        self.result = result
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if (self.result == Exception):
+            print(f"Test: {self.name} got EXCEPTION {exc_type.__name__}: {exc_value}")
+        print(f"Test: {self.name} is {'OK' if (self.result==True) else'FAILED'} in {time.time() - self.init_time} seconds")
+
+def test_decorator(func):
+    def func_wrapper(*args, **kwargs):
+        with EnterExitLog(func.__name__) as eel:
+            try:
+                result = func(*args, **kwargs)
+                eel.set_result(result)
+                return result
+            except Exception as e:
+                eel.set_result(e)
+                return False
+    return func_wrapper
+
+def python_return():
+   print("hi")
+
+@test_decorator
+def test_function_call():
+    result = mplibmad.hello("abc")
+    expected = python_return()
+    return (result == expected)
+
+@test_decorator
+def test_type_error():
+    """testing with integer instead of string"""
     try:
-      mplibmad.hello(42)
+      r = mplibmad.hello(42)
+      print(f"ERROR: expected TypeError but got result: {r}")
     except TypeError:
-      print(f"got expected TypeError exception, working as designed")
+        return True
     except Exception as e:
       print(f"got unexpected exception: {type(e).__name__}: {e.value}")
+    return False
 
 def my_callback(tmp):
-    print("in my_callback")
-    print(f"tmp was: {tmp}")
+    print(f"callback got: {tmp}")
     return 123
 
-def testCallback():
-    mplibmad.set_cb(my_callback)
-    mplibmad.call_cb(42)
+@test_decorator
+def test_callback():
+    r = mplibmad.set_cb(my_callback)
+    assert r is None, "set_cb should return None"
+    r = mplibmad.call_cb("hi there")
+    assert r is 123, "call_cb should return 123"
+    return True
 
-print("Done.")
+def run_tests():
+    print("Start Test:")
+    test_function_call()
+    test_type_error()
+    test_callback()
+    print("Done.")
+    
+if __name__ == "__main__":
+    run_tests()
